e7ab5f3d452fdd0014b8f3380638dcfeb8b4f7af
trajan <trajan.bronner@gmail.com>
2020-08-21 11:47:34 +0200

    React

---
+++ precache-manifest.e3a88af9f36743bfd59f24bd59e1f319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f99c090d6b660aca281b7050456481a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f05ad566ef158710bcc",
+    "url": "/static/css/main.38f70c07.chunk.css"
+  },
+  {
+    "revision": "58ae1b9205ad7284d2e1",
+    "url": "/static/js/2.692f05f5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.692f05f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f05ad566ef158710bcc",
+    "url": "/static/js/main.cac35821.chunk.js"
+  },
+  {
+    "revision": "2611ea67ee70f0b8139c",
+    "url": "/static/js/runtime-main.1ee2e652.js"
+  }
+]);
